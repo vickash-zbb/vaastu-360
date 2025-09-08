c86d0e5e24529feaf82776cc018027c6b56a3fe1
vickash-zbb <vickashshivanzbb@gmail.com>
2025-09-08 15:37:59 +0530

    login signup ui change

--- client/components/layout/Layout.tsx
+++ client/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 bg-gradient-to-br from-background to-muted/40  ">
+      <main className="flex-1 from-background to-muted/40  ">
         <div className=" mx-auto w-full">
           <Outlet />
         </div>
